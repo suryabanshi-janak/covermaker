ffdd47da26ede7f40a2682d8fd1c67f8a0b64f11
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-06-14 10:42:57 +0545

    file upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Recursive } from 'next/font/google';
 import './globals.css';
+
+import { Toaster } from '@/components/ui/toaster';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
@@ -20,8 +22,13 @@ export default function RootLayout({
     <html lang='en'>
       <body className={recursive.className}>
         <Navbar />
-        <main>{children}</main>
-        <Footer />
+
+        <main className='flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]'>
+          <div className='flex-1 flex flex-col h-full'>{children}</div>
+          <Footer />
+        </main>
+
+        <Toaster />
       </body>
     </html>
   );
