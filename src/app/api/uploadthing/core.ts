d9577dcde798f1ea0d8ec1d89f4ac6a388bcfa16
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-06-17 10:11:37 +0545

    db setup

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,5 +1,7 @@
 import { createUploadthing, type FileRouter } from 'uploadthing/next';
 import { z } from 'zod';
+import sharp from 'sharp';
+import { db } from '@/db';
 
 const f = createUploadthing();
 
@@ -12,7 +14,34 @@ export const ourFileRouter = {
     .onUploadComplete(async ({ metadata, file }) => {
       const { configId } = metadata.input;
 
-      return { configId };
+      const res = await fetch(file.url);
+      const buffer = await res.arrayBuffer();
+
+      const imgMetadata = await sharp(buffer).metadata();
+      const { height, width } = imgMetadata;
+
+      if (!configId) {
+        const configuration = await db.configuration.create({
+          data: {
+            height: height || 500,
+            width: width || 500,
+            imageUrl: file.url,
+          },
+        });
+
+        return { configId: configuration.id };
+      } else {
+        const updatedConfiguration = await db.configuration.update({
+          where: {
+            id: configId,
+          },
+          data: {
+            croppedImageUrl: file.url,
+          },
+        });
+
+        return { configId: updatedConfiguration };
+      }
     }),
 } satisfies FileRouter;
 
--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -1,12 +1,12 @@
+import { ReactNode } from 'react';
+
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
 import Steps from '@/components/Steps';
-import { ReactNode } from 'react';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <MaxWidthWrapper className='flex-1 flex flex-col'>
       <Steps />
-
       {children}
     </MaxWidthWrapper>
   );
