d0d6275642b778f92228a43dd1a0951f26a2c81d
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-06-18 08:47:15 +0545

    image configurator

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,3 +1,37 @@
-export default async function DesignPage() {
-  return <div>page</div>;
+import { db } from '@/db';
+import { notFound } from 'next/navigation';
+import DesignConfigurator from './DesignConfigurator';
+
+interface DesignPageProps {
+  searchParams: {
+    [key: string]: string;
+  };
+}
+
+export default async function DesignPage({ searchParams }: DesignPageProps) {
+  const { id } = searchParams;
+
+  if (!id) {
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { height, width, imageUrl } = configuration;
+
+  return (
+    <DesignConfigurator
+      confidId={id}
+      imageUrl={imageUrl}
+      imageDimensions={{ height, width }}
+    />
+  );
 }
